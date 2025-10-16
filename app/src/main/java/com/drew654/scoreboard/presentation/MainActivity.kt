564641dd21d74e54aee5984a5aa6e968155fe2d1
Drew Pusey <drewpusey654@gmail.com>
2025-10-16 07:03:24 -0500

    Fix inner padding

--- app/src/main/java/com/drew654/scoreboard/presentation/MainActivity.kt
+++ app/src/main/java/com/drew654/scoreboard/presentation/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,16 +20,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             ScoreboardTheme {
-                val navController = rememberNavController()
-                NavHost(
-                    navController = navController,
-                    startDestination = "${Screen.Scoreboard.route}/football/college-football"
-                ) {
-                    composable(
-                        route = "${Screen.Scoreboard.route}/{sport}/{league}"
+                Scaffold { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "${Screen.Scoreboard.route}/football/college-football",
+                        modifier = Modifier.padding(innerPadding)
                     ) {
-                        ScoreboardScreen()
+                        composable(
+                            route = "${Screen.Scoreboard.route}/{sport}/{league}"
+                        ) {
+                            ScoreboardScreen()
+                        }
                     }
                 }
             }
