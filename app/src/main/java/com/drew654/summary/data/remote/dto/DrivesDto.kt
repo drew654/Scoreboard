7c358885796e8d6efc643596816f6fcf1d2552c3
Drew Pusey <drewpusey654@gmail.com>
2025-12-06 20:52:52 -0600

    Fix current drive

--- app/src/main/java/com/drew654/summary/data/remote/dto/DrivesDto.kt
+++ app/src/main/java/com/drew654/summary/data/remote/dto/DrivesDto.kt
@@ -3,13 +3,13 @@ package com.drew654.summary.data.remote.dto
 import com.drew654.summary.domain.model.summary.Drives
 
 data class DrivesDto(
-    val current: CurrentDriveDto,
+    val current: CurrentDriveDto?,
     val previous: List<DriveDto>
 )
 
 fun DrivesDto.toDrives(): Drives {
     return Drives(
-        current = current.toCurrentDrive(),
+        current = current?.toCurrentDrive(),
         previous = previous.map { it.toDrive() }
     )
 }
